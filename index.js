5134d273566437d9e8b731cfbc4f19c4d11b63e7
Samirun-Shuvo <samirunshuvo@gmail.com>
2024-06-12 11:02:32 +0600

    update connection

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster2.ubetca2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2`;
 
-//connection
+
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
